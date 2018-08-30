ee4164be496e321b1081e168bb4b5eaf1276d58a
Randula Koralage <randulakoralage82@gmail.com>
2018-08-30 15:24:50 +0530

    Add files via upload

---
+++ src/externalsoftwares/ExternalSoftwares.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package externalsoftwares;
+
+import imageviewer.ImageViewerSof;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import mediaplayer.MediaPlayerSof;
+import notepad.NotePadSof;
+import notepad.PenDrive;
+import pdfviewer.PDFViewerSof;
+
+/**
+ *
+ * @author Randula
+ */
+public class ExternalSoftwares {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+//        ImageViewerSof im = new ImageViewerSof("C:\\Users\\Randula\\Desktop\\libraryP\\Screen\\Sample");
+//        MediaPlayerSof mp = new MediaPlayerSof("123.mp4");
+//        NotePadSof np = new NotePadSof();
+//
+//        try {
+//            PDFViewerSof pd = new PDFViewerSof("2.pdf");
+//        } catch (IOException ex) {
+//            Logger.getLogger(ExternalSoftwares.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+
+    }
+
+}
